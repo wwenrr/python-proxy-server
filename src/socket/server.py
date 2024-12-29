725ccacdd330fb2a217e6b4a44d084bdce8a2933
root <root@instance-20241229-075710.asia-east2-c.c.annular-aria-439110-b1.internal>
2024-12-29 22:17:27 +0000

    add domain

--- src/socket/server.py
+++ src/socket/server.py
@@ -3,6 +3,7 @@ import time
 import signal
 import sys
 import threading
+import ssl
 
 http_wait = """HTTP/1.1 200 OK
 Content-Type: text/plain
@@ -17,17 +18,19 @@ Giới hạn kết nối
 
 connect = 0
 max_connection = 5
+CERT_FILE = "/etc/letsencrypt/live/hcmutssps.id.vn/fullchain.pem"
+KEY_FILE = "/etc/letsencrypt/live/hcmutssps.id.vn/privkey.pem"
 lock = threading.Lock()
 
 def handle_request(client_socket, client_address):
     global connect
 
     try:
-        # rev = client_socket.recv(2048).decode('utf-8')
+        rev = client_socket.recv(2048).decode('utf-8')
 
         if(connect >= max_connection):
             print("Giới hạn kết nối, kết nối thất bại!\n")
-            client_socket.send(http_error.encode())
+            client_socket.send(http_error.encode('utf-8'))
             client_socket.close()
         else:
             # print(f"Request receive:\n {rev}")
@@ -37,9 +40,9 @@ def handle_request(client_socket, client_address):
             
             client_socket.send(http_wait.encode())
             time.sleep(1)
-            client_socket.send("chờ xíu bro\n".encode())
+            client_socket.send("chờ xíu bro\n".encode('utf-8'))
             time.sleep(5)
-            client_socket.send("chờ xong r".encode())
+            client_socket.send("hello world".encode('utf-8'))
     except BrokenPipeError:
         print("Client hủy kết nối đột ngột")
     except OSError as e:
@@ -51,7 +54,7 @@ def handle_request(client_socket, client_address):
         with lock:  
                 connect -= 1
 
-def start_server(host='0.0.0.0', max_connec = 5, port=80):
+def start_server(host='0.0.0.0', max_connec = 5, port=443):
     global max_connection
     max_connection = max_connec
 
@@ -60,6 +63,13 @@ def start_server(host='0.0.0.0', max_connec = 5, port=80):
     server_socket.listen(max_connection)
     print(f"Server is listening on {host}:{port}")
     print(f"Số host tối đa: {max_connec}")
+
+    # Chuyển socket sang HTTPS với chứng chỉ SSL
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.load_cert_chain(certfile="/etc/letsencrypt/live/hcmutssps.id.vn/fullchain.pem",
+                            keyfile="/etc/letsencrypt/live/hcmutssps.id.vn/privkey.pem")
+
+    server_socket = context.wrap_socket(server_socket, server_side=True)
 
     def handle_signal(signum, frame):
         print("\nServer is shutting down...")
